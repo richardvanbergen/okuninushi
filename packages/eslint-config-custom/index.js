e37ac8103dabf4eb34d9dcf83c8490bcea44f746
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-09-16 10:37:23 +0900

    enforce single quotes

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,9 +1,10 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ['next', 'turbo', 'prettier'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "semi": ["error", "never"],
-    "react/jsx-key": "off",
-    "object-curly-spacing": ["error", "always"]
+    '@next/next/no-html-link-for-pages': 'off',
+    'semi': ['error', 'never'],
+    'react/jsx-key': 'off',
+    'object-curly-spacing': ['error', 'always'],
+    'quotes': ['error', 'single'],
   },
 }
