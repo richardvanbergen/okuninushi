539bd0c062790b7598c59000a8785fa95a328146
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-09-16 10:32:29 +0900

    disable semis and enforce spaces in braces

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: ["next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "semi": ["error", "never"],
     "react/jsx-key": "off",
+    "object-curly-spacing": ["error", "always"]
   },
-};
+}
